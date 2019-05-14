0ed605d4828cd4a00cbf0362d228fe73166ee79e
Jeremy Soller <jeremy@system76.com>
2019-05-13 21:05:29 -0600

    Update daemon to improve reliability

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -14,7 +14,7 @@ fn syscall_error(error: syscall::Error) -> io::Error {
     io::Error::from_raw_os_error(error.errno)
 }
 
-pub fn handle(event_file: &mut File, master_fd: RawFd, process: &mut Child) -> io::Result<ExitStatus> {
+pub fn handle(event_file: &mut File, master_fd: RawFd, timeout_fd: RawFd, process: &mut Child) -> io::Result<ExitStatus> {
     let handle_event = |event_id: RawFd| -> io::Result<bool> {
         if event_id == master_fd {
             let mut packet = [0; 4096];
@@ -30,6 +30,14 @@ pub fn handle(event_file: &mut File, master_fd: RawFd, process: &mut Child) -> i
                     Pio::<u8>::new(0xe9).write(packet[i]);
                 }
             }
+        } else if event_id == timeout_fd {
+            let mut timespec = syscall::TimeSpec::default();
+            syscall::read(timeout_fd as usize, &mut timespec).map_err(syscall_error)?;
+
+            timespec.tv_sec += 1;
+            syscall::write(timeout_fd as usize, &mut timespec).map_err(syscall_error)?;
+
+            Ok(true)
         } else {
             Err(io::Error::new(
                 io::ErrorKind::InvalidData,
@@ -38,14 +46,8 @@ pub fn handle(event_file: &mut File, master_fd: RawFd, process: &mut Child) -> i
         }
     };
 
-    if handle_event(master_fd)? {
+    if handle_event(master_fd)? && handle_event(timeout_fd)? {
         'events: loop {
-            let mut sys_event = syscall::Event::default();
-            event_file.read(&mut sys_event)?;
-            if ! handle_event(sys_event.id as RawFd)? {
-                break 'events;
-            }
-
             match process.try_wait() {
                 Ok(status_opt) => match status_opt {
                     Some(status) => return Ok(status),
@@ -55,6 +57,12 @@ pub fn handle(event_file: &mut File, master_fd: RawFd, process: &mut Child) -> i
                     io::ErrorKind::WouldBlock => (),
                     _ => return Err(err),
                 }
+            }
+
+            let mut sys_event = syscall::Event::default();
+            event_file.read(&mut sys_event)?;
+            if ! handle_event(sys_event.id as RawFd)? {
+                break 'events;
             }
         }
     }
@@ -89,6 +97,9 @@ fn inner() -> io::Result<()> {
     let (columns, lines) = (DEFAULT_COLS, DEFAULT_LINES);
     let (master_fd, pty) = getpty(columns, lines)?;
 
+    let timeout_fd = syscall::open("time:4", syscall::O_CLOEXEC | syscall::O_RDWR | syscall::O_NONBLOCK)
+        .map_err(syscall_error)? as RawFd;
+
     let mut event_file = OpenOptions::new()
         .read(true)
         .write(true)
@@ -96,6 +107,12 @@ fn inner() -> io::Result<()> {
 
     event_file.write(&syscall::Event {
         id: master_fd as usize,
+        flags: syscall::flag::EVENT_READ,
+        data: 0
+    })?;
+
+    event_file.write(&syscall::Event {
+        id: timeout_fd as usize,
         flags: syscall::flag::EVENT_READ,
         data: 0
     })?;
@@ -121,7 +138,7 @@ fn inner() -> io::Result<()> {
         }
 
         let mut process = command.spawn()?;
-        let status = handle(&mut event_file, master_fd, &mut process)?;
+        let status = handle(&mut event_file, master_fd, timeout_fd, &mut process)?;
         if status.success() {
             Ok(())
         } else {
