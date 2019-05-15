9b7dbe5f6e34b7e4ab59577a703ad8aaec4af4e7
Jeremy Soller <jeremy@system76.com>
2019-05-14 21:17:07 -0600

    0.2.1 - remove warnings

--- src/install.rs
+++ src/install.rs
@@ -1,7 +1,6 @@
 use std::{io, process};
-use std::process::Command;
 
-use crate::{installed, status_error, toolchain};
+use crate::toolchain;
 
 fn inner() -> io::Result<()> {
     let _toolchain_dir = toolchain()?;
