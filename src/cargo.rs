ada673454ffc4eee6c28748ddf9f03feb14ffe1a
Jeremy Soller <jeremy@system76.com>
2019-06-19 16:33:44 -0600

    Accept --gui for cargo commands

--- src/cargo.rs
+++ src/cargo.rs
@@ -31,13 +31,35 @@ fn inner() -> io::Result<()> {
     let command = args.next().unwrap();
     let subcommand = args.next().unwrap();
 
+    let mut arguments = Vec::new();
+    let mut matching = true;
+    let mut gui = false;
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "-g" | "--gui" if matching => {
+                gui = true;
+            },
+            "--" if matching => {
+                matching = false;
+                arguments.push(arg);
+            }
+            _ => {
+                arguments.push(arg);
+            }
+        }
+    }
+
     // TODO: Ensure no spaces in command
-    let runner = format!("{} exec --folder .", command);
+    let runner = format!(
+        "{} exec --folder .{}",
+        command,
+        if gui { " --gui" } else { "" }
+    );
 
     Command::new("cargo")
         .arg(subcommand)
         .arg("--target").arg(target)
-        .args(args)
+        .args(arguments)
         .env("CARGO_TARGET_X86_64_UNKNOWN_REDOX_LINKER", linker)
         .env("CARGO_TARGET_X86_64_UNKNOWN_REDOX_RUNNER", runner)
         .env("PATH", path)
