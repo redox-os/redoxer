a20b7a241e8c20661a90b2015ea845de346231d2
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-08-30 16:17:20 +0200

    Switch redoxerd to anyhow for better error messages
    Also delay exit for a bit to ensure the tty subsystem gets a chance to
    flush the written error.

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,6 +1,4 @@
-extern crate redox_termios;
-extern crate syscall;
-
+use anyhow::Context;
 use std::fs::{self, File, OpenOptions};
 use std::io::{self, Read, Write};
 use std::os::unix::io::{FromRawFd, IntoRawFd, RawFd};
@@ -47,14 +45,20 @@ unsafe fn sys_call<T>(
 //   a. this function should be moved to libredox
 //   b. move the qemu test driver part to drivers repo and let redoxerd communicate with that driver instead
 fn acquire_port_io_rights() -> io::Result<()> {
-    let kernel_fd = syscall::dup(unsafe { redox_cur_thrfd_v0() }, b"open_via_dup").map_err(syscall_error)?;
+    let kernel_fd =
+        syscall::dup(unsafe { redox_cur_thrfd_v0() }, b"open_via_dup").map_err(syscall_error)?;
     let res = unsafe { sys_call::<[u8; 0]>(kernel_fd, &mut [], 0, &[ProcSchemeVerb::Iopl as u64]) };
     let _ = syscall::close(kernel_fd);
     res?;
     Ok(())
 }
 
-pub fn handle(event_file: &mut File, master_fd: RawFd, timeout_fd: RawFd, process: &mut Child) -> io::Result<ExitStatus> {
+pub fn handle(
+    event_file: &mut File,
+    master_fd: RawFd,
+    timeout_fd: RawFd,
+    process: &mut Child,
+) -> io::Result<ExitStatus> {
     let handle_event = |event_id: RawFd| -> io::Result<bool> {
         if event_id == master_fd {
             let mut packet = [0; 4096];
@@ -86,7 +90,7 @@ pub fn handle(event_file: &mut File, master_fd: RawFd, timeout_fd: RawFd, proces
         } else {
             Err(io::Error::new(
                 io::ErrorKind::InvalidData,
-                format!("unexpected event id {}", event_id)
+                format!("unexpected event id {}", event_id),
             ))
         }
     };
@@ -96,17 +100,17 @@ pub fn handle(event_file: &mut File, master_fd: RawFd, timeout_fd: RawFd, proces
             match process.try_wait() {
                 Ok(status_opt) => match status_opt {
                     Some(status) => return Ok(status),
-                    None => ()
+                    None => (),
                 },
                 Err(err) => match err.kind() {
                     io::ErrorKind::WouldBlock => (),
                     _ => return Err(err),
-                }
+                },
             }
 
             let mut sys_event = syscall::Event::default();
             event_file.read(&mut sys_event)?;
-            if ! handle_event(sys_event.id as RawFd)? {
+            if !handle_event(sys_event.id as RawFd)? {
                 break 'events;
             }
         }
@@ -117,33 +121,44 @@ pub fn handle(event_file: &mut File, master_fd: RawFd, timeout_fd: RawFd, proces
 }
 
 pub fn getpty(columns: u32, lines: u32) -> io::Result<(RawFd, String)> {
-    let master = syscall::open("/scheme/pty", syscall::O_CLOEXEC | syscall::O_RDWR | syscall::O_CREAT | syscall::O_NONBLOCK)
-        .map_err(syscall_error)?;
+    let master = syscall::open(
+        "/scheme/pty",
+        syscall::O_CLOEXEC | syscall::O_RDWR | syscall::O_CREAT | syscall::O_NONBLOCK,
+    )
+    .map_err(syscall_error)?;
 
     if let Ok(winsize_fd) = syscall::dup(master, b"winsize") {
-        let _ = syscall::write(winsize_fd, &redox_termios::Winsize {
-            ws_row: lines as u16,
-            ws_col: columns as u16
-        });
+        let _ = syscall::write(
+            winsize_fd,
+            &redox_termios::Winsize {
+                ws_row: lines as u16,
+                ws_col: columns as u16,
+            },
+        );
         let _ = syscall::close(winsize_fd);
     }
 
     let mut buf: [u8; 4096] = [0; 4096];
     let count = syscall::fpath(master, &mut buf).map_err(syscall_error)?;
-    Ok((master as RawFd, unsafe { String::from_utf8_unchecked(Vec::from(&buf[..count])) }))
+    Ok((master as RawFd, unsafe {
+        String::from_utf8_unchecked(Vec::from(&buf[..count]))
+    }))
 }
 
-fn inner() -> io::Result<()> {
+fn inner() -> anyhow::Result<()> {
     acquire_port_io_rights()?;
 
-    let config = fs::read_to_string("/etc/redoxerd")?;
+    let config = fs::read_to_string("/etc/redoxerd").context("Failed to read /etc/redoxerd")?;
     let mut config_lines = config.lines();
 
     let (columns, lines) = (DEFAULT_COLS, DEFAULT_LINES);
     let (master_fd, pty) = getpty(columns, lines)?;
 
-    let timeout_fd = syscall::open("/scheme/time/4", syscall::O_CLOEXEC | syscall::O_RDWR | syscall::O_NONBLOCK)
-        .map_err(syscall_error)? as RawFd;
+    let timeout_fd = syscall::open(
+        "/scheme/time/4",
+        syscall::O_CLOEXEC | syscall::O_RDWR | syscall::O_NONBLOCK,
+    )
+    .map_err(syscall_error)? as RawFd;
 
     let mut event_file = OpenOptions::new()
         .read(true)
@@ -153,26 +168,28 @@ fn inner() -> io::Result<()> {
     event_file.write(&syscall::Event {
         id: master_fd as usize,
         flags: syscall::flag::EVENT_READ,
-        data: 0
+        data: 0,
     })?;
 
     event_file.write(&syscall::Event {
         id: timeout_fd as usize,
         flags: syscall::flag::EVENT_READ,
-        data: 0
+        data: 0,
     })?;
 
     let slave_stdin = OpenOptions::new().read(true).open(&pty)?;
     let slave_stdout = OpenOptions::new().write(true).open(&pty)?;
     let slave_stderr = OpenOptions::new().write(true).open(&pty)?;
 
-    if let Some(name) = config_lines.next() {
-        let mut command = Command::new(name);
-        for arg in config_lines {
-            command.arg(arg);
-        }
-        unsafe {
-            command
+    let Some(name) = config_lines.next() else {
+        anyhow::bail!("/etc/redoxerd does not specify command");
+    };
+    let mut command = Command::new(name);
+    for arg in config_lines {
+        command.arg(arg);
+    }
+    unsafe {
+        command
             .stdin(Stdio::from_raw_fd(slave_stdin.into_raw_fd()))
             .stdout(Stdio::from_raw_fd(slave_stdout.into_raw_fd()))
             .stderr(Stdio::from_raw_fd(slave_stderr.into_raw_fd()))
@@ -180,23 +197,17 @@ fn inner() -> io::Result<()> {
             .env("LINES", format!("{}", lines))
             .env("TERM", "xterm-256color")
             .env("TTY", &pty);
-        }
+    }
 
-        let mut process = command.spawn()?;
-        let status = handle(&mut event_file, master_fd, timeout_fd, &mut process)?;
-        if status.success() {
-            Ok(())
-        } else {
-            Err(io::Error::new(
-                io::ErrorKind::Other,
-                format!("{}", status)
-            ))
-        }
+    let mut process = command
+        .spawn()
+        .with_context(|| format!("Failed to spawn {command:?}"))?;
+    let status = handle(&mut event_file, master_fd, timeout_fd, &mut process)
+        .with_context(|| format!("Failed to run {name}"))?;
+    if status.success() {
+        Ok(())
     } else {
-        Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            "/etc/redoxerd does not specify command"
-        ))
+        anyhow::bail!("{name} failed with {}", status);
     }
 }
 
@@ -206,9 +217,13 @@ pub fn main() {
             // Exit with success using qemu device
             Pio::<u16>::new(0x604).write(0x2000);
             Pio::<u8>::new(0x501).write(51 / 2);
-        },
+        }
         Err(err) => {
-            eprintln!("redoxerd: {}", err);
+            eprintln!("redoxerd: {:#}", err);
+
+            // Wait a bit for the error message to get flushed through the tty subsystem before exiting.
+            std::thread::sleep(std::time::Duration::from_millis(100));
+
             // Exit with error using qemu device
             Pio::<u8>::new(0x501).write(53 / 2);
         }
